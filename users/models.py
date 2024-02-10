e6f883ef1ac8d86cbaa3d71ec235cbca512853b9
Ali Bayani <aliwbayani@gmail.com>
2024-02-10 19:23:22 +0330

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from django.utils import timezone
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    Bio   =  models.TextField()
+    JoinDate = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
